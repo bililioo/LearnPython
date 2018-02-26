c240593fa481f8d1642b3e392d0410ba7ae804b1
KKucar's <kkucars@KKucars-Macintosh.local>
2018-02-26 18:30:01 +0800

    -
    Signed-off-by: KKucar's <kkucars@KKucars-Macintosh.local>

--- learn_psutil.py
+++ learn_psutil.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+
+import psutil
+
+print(psutil.cpu_count())
+print(psutil.cpu_count(logical=False))
+
+for x in range(10):
+    print(psutil.cpu_times())
+
+print(psutil.virtual_memory())
+print(psutil.swap_memory())
+
+print(psutil.disk_partitions())
