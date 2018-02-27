8153fbf2a8dc8ca3866c6c5a47f12400bcfa82b1
KKucar's <kkucars@KKucars-Macintosh.local>
2018-02-27 10:39:51 +0800

    insert tkinter.py

--- learn_Tkinter.py
+++ learn_Tkinter.py
@@ -3,6 +3,25 @@
 # import tkinter
 from tkinter import *
 
+
+# class Application(Frame):
+#     def __init__(self, master=None):
+#         Frame.__init__(self, master)
+#         self.pack()
+#         self.createWidgets()
+
+#     def createWidgets(self):
+#         self.helloLabel = Label(self, text='hello world')
+#         self.helloLabel.pack()
+#         self.quitBtn = Button(self, text='quit', command=self.quit)
+#         self.quitBtn.pack()
+
+# app = Application()
+# app.master.title('hello')
+# app.mainloop()
+
+import tkinter.messagebox as messagebox
+
 class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
@@ -10,11 +29,19 @@ class Application(Frame):
         self.createWidgets()
 
     def createWidgets(self):
-        self.helloLabel = Label(self, text='hello world')
-        self.helloLabel.pack()
-        self.quitBtn = Button(self, text='quit', command=self.quit)
+        self.nameIp = Entry(self)
+        self.nameIp.pack()
+
+        self.alertButton = Button(self, text='hello', command=self.hello)
+        self.alertButton.pack()
+        
+        self.quitBtn = Button(self, text='exit', command=self.quit)
         self.quitBtn.pack()
+    
+    def hello(self):
+        name = self.nameIp.get() or 'world'
+        messagebox.showinfo('message', 'hello %s' % name)
 
 app = Application()
-app.master.title('hello')
+app.master.title('test')
 app.mainloop()
